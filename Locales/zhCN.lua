fdcf21c7912a1533d7b9fe098972fa1937c62366
phyber <phyber@localhost>
2012-09-06 14:20:29 +0100

    zhCN.lua: Fixes. Some strings still missing.

--- Locales/zhCN.lua
+++ Locales/zhCN.lua
@@ -1,33 +1,30 @@
 local L = LibStub("AceLocale-3.0"):NewLocale("Broker_MoneyFu", "zhCN")
 if not L then return end
 --
-L["NAME"] = "FuBar - MoneyFu"
-L["DESCRIPTION"] = "记录你所在服务器上所有角色的金钱状况."
-L["COMMANDS"] = {"/monfu", "/moneyfu"}
-L["TEXT_TOTAL"] = "总计"
-L["TEXT_SESSION_RESET"] = "重置本次连接统计"
-L["TEXT_THIS_SESSION"] = "本次连接"
-L["TEXT_GAINED"] = "盈利"
-L["TEXT_SPENT"] = "消费"
-L["TEXT_AMOUNT"] = "总计"
-L["TEXT_PER_HOUR"] = "每小时"
+L["Total"] = "总计"
+L["Session reset"] = "重置本次连接统计"
+L["This session"] = "本次连接"
+L["Per hour"] = "每小时"
 L["This Week"] = "本周"
+-- Yellow texts
+L["|cffffff00Gained|r"] = "盈利"
+L["|cffffff00Spent|r"] = "消费"
+L["|cffffff00Profit|r"] = "盈利"
+L["|cffffff00Loss|r"] = "消费"
 
-L["ARGUMENT_RESETSESSION"] = "resetSession"
+L["Amount"] = "总计"
+L["Reset Session"] = "重置本次连接统计"
+L["Show character-specific cashflow"] = "只显示本角色金钱状况"
+L["Purge"] = "清空"
+L["Graphical"] = "硬币图标显示"
+L["Full"] = "完全样式显示"
+L["Short"] = "简洁样式显示"
+L["Condensed"] = "紧缩样式显示"
+L["Simplified Tooltip"] = "简单提示"
+L["Show per hour cashflow"] = "显示每小时金钱状况"
 
-L["MENU_RESET_SESSION"] = "重置本次连接统计"
-L["MENU_CHARACTER_SPECIFIC_CASHFLOW"] = "只显示本角色金钱状况"
-L["MENU_PURGE"] = "清空"
-L["MENU_SHOW_GRAPHICAL"] = "硬币图标显示"
-L["MENU_SHOW_FULL"] = "完全样式显示"
-L["MENU_SHOW_SHORT"] = "简洁样式显示"
-L["MENU_SHOW_CONDENSED"] = "紧缩样式显示"
-L["SIMPLIFIED_TOOLTIP"] = "简单提示"
-L["SHOW_PER_HOUR_CASHFLOW"] = "显示每小时金钱状况"
+L["Session reset."] = "重置本次连接统计."
+L["Characters"] = "角色"
 
-L["TEXT_SESSION_RESET"] = "重置本次连接统计."
-L["TEXT_CHARACTERS"] = "角色"
-L["TEXT_PROFIT"] = "盈利"
-L["TEXT_LOSS"] = "消费"
-
-L["HINT"] = "点击提取金钱"
+-- Green texts
+L["|cff00ff00Click to pick up money|r"] = "点击提取金钱"
